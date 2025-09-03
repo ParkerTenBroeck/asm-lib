ddc9e2829b3fe1171b2274267e1471f45a0733a7
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-09-03 16:44:21 -0400

    log of 0 lol

--- src/simple/trans/display.rs
+++ src/simple/trans/display.rs
@@ -77,7 +77,7 @@ impl<T: PrimInt + LowerHex> Symbol<T> {
             )?;
         }
         if let Some(dbg) = trans.symbols.get_symbol_dbg(idx) {
-            let list_size = trans.symbols.len().ilog10() as usize + 1;
+            let list_size = trans.symbols.len().checked_ilog10().map(|v| v as usize +1).unwrap_or(0);
             if let Some(def) = &dbg.definition {
                 write!(
                     f,
@@ -129,7 +129,7 @@ impl<T: PrimInt + LowerHex> Symbols<T> {
         let visibility = "Vis";
         let section = "Section";
         let name = "Name";
-        let list_size = trans.symbols.len().ilog10() as usize + 1;
+        let list_size = trans.symbols.len().checked_ilog10().map(|v| v as usize +1).unwrap_or(0);
         writeln!(
             f,
             "{:list_size$}  {offset: >int_size$} {size: >int_size$} {kind:10} {visibility:6} {section:10} {name}",
